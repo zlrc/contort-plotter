f08c92ad881296e46e7fdf858a1337f8444aed4c
Mike K <30582227+zlrc@users.noreply.github.com>
2022-06-24 00:26:32 -0700

    refactor(GUI): style modifications to SquareButton

--- src/components/SquareButton.vue
+++ src/components/SquareButton.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 const props = defineProps<{
-    color?: string
+    fillColor?: string
+    outlineColor?: string
 }>();
 
-const color = props.color ? props.color : "white";
+const color = props.fillColor ? props.fillColor : "white";
+const outline = props.outlineColor ? props.outlineColor : "black";
 </script>
 
 <!---->
@@ -17,8 +19,9 @@ const color = props.color ? props.color : "white";
 <style scoped lang="scss">
     .button {
         height: inherit;
+        min-height: 1.75rem;
         aspect-ratio: 1/1;
         background: v-bind(color);
-        border: solid 0.25rem black;
+        border: solid 0.25rem v-bind(outline);
     }
 </style>
