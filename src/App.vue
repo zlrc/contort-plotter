221e30316a0ed10af158793b3e56a4940adfc4be
Mike K <30582227+zlrc@users.noreply.github.com>
2022-07-20 01:03:41 -0700

    refactor: moved css reset code over to reset.scss

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue';
 import { AppCanvas, AppGUI } from '@/components';
 import { GraphCalculator } from '@/scene';
+import '@/styles/reset.scss';
 
 const calculator = ref<GraphCalculator>();
 
@@ -20,43 +21,6 @@ function onCanvasReady(calc : GraphCalculator) {
 <!---->
 
 <style lang="scss">
-// CSS Reset by Josh W. Comeau: https://www.joshwcomeau.com/css/custom-css-reset/
-
-*, *::before, *::after {
-  box-sizing: border-box;
-}
-
-* {
-  margin: 0;
-}
-
-html, body, #app {
-  height: 100%;
-}
-
-body {
-  line-height: 1.5;
-  -webkit-font-smoothing: antialiased;
-}
-
-img, picture, video, canvas, svg {
-  display: block;
-  max-width: 100%;
-}
-
-input, button, textarea, select {
-  font: inherit;
-}
-
-p, h1, h2, h3, h4, h5, h6 {
-  overflow-wrap: break-word;
-}
-
-#root, #__next {
-  isolation: isolate;
-}
-
-// Custom styles
 #app {
   display: flex;
   flex-direction: column;
