392af17ec98d28e483fb273356b2bdb8d0bf779a
Mike K <30582227+zlrc@users.noreply.github.com>
2022-07-29 01:30:41 -0700

    feat: added constants.ts

---
+++ src/utils/constants.ts
@@ -0,0 +1,11 @@
+export const menu = {
+    "Modifiers": [
+        {name: "Parabola", icon: "parabola"},
+        {name: "Wave", icon: "wave"},
+        {name: "Step", icon: "step"},
+    ],
+    "Advanced": [
+        {name: "CustomExpr", icon: ""},
+        {name: "Config", icon: "ellipsis"}
+    ]
+}
