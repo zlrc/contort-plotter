3008e0b7c46efdbab89c2161cf61640baccd1392
Mike K <30582227+zlrc@users.noreply.github.com>
2022-07-29 01:53:58 -0700

    feat: FlagButton icons are now auto-assigned color

--- src/components/FlagButton.vue
+++ src/components/FlagButton.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
 import { SVGIcon } from '@/components';
+import { contrastStr } from '@/utils/colors';
 
 const props = defineProps<{
     /** @default white */
@@ -10,17 +12,27 @@ const props = defineProps<{
     icon?: string;
 }>();
 
+// Props
 const color = props.fillColor ? props.fillColor : "white";
 const outline = props.outlineColor ? props.outlineColor : "black";
+
+// References
+const iconColor = ref<string>("");
+const pathBG = ref<HTMLElement>();
+
+onMounted(() => {
+    if (props.icon)
+        iconColor.value = contrastStr(getComputedStyle(pathBG.value as HTMLElement).fill);
+});
 </script>
 
 <!---->
 
 <template>
     <button class="button" @contextmenu.prevent="">
-        <SVGIcon class="button__icon" v-if="props.icon" :name="props.icon" />
+        <SVGIcon class="button__icon" v-if="props.icon && iconColor" :name="props.icon" :color="iconColor" />
         <svg viewBox="0 0 140 69" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path id="path-bg" d="M1 68V1H103.086L136.586 34.5L103.086 68H34.5H1Z" />
+            <path ref="pathBG" id="path-bg" d="M1 68V1H103.086L136.586 34.5L103.086 68H34.5H1Z" />
         </svg>
     </button>
 </template>
