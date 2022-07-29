3de455d2eff62f2f3ef2be8da5a7ef0ddc2e76c9
Mike K <30582227+zlrc@users.noreply.github.com>
2022-07-29 01:32:28 -0700

    feat: added title prop to ToolboxSection.vue

--- src/components/ToolboxSection.vue
+++ src/components/ToolboxSection.vue
@@ -1,10 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const props = defineProps<{
+    /** The section's header text. */
+    title?: string
+}>();
+</script>
 
 <!---->
 
 <template>
     <div class="Toolbox__section">
-        <h3></h3>
+        <h3 v-if="props.title">{{props.title}}</h3>
         <slot />
     </div>
 </template>
