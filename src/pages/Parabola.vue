da6f81fe301fc3f881dcffb21f6675d75c4e7c66
Mike K <30582227+zlrc@users.noreply.github.com>
2022-07-29 22:16:49 -0700

    feat: added unfinished pages for Parabola and Wave

---
+++ src/pages/Parabola.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { ModifierPage, Slider } from '@/components';
+
+const xParabola = ref(0.5);
+const template = ref(`{expr} + ${xParabola.value}*x^2`);
+
+function onSliderInput() {
+    template.value = `{expr} + ${xParabola.value}*x^2`;
+}
+</script>
+
+<!---->
+
+<template>
+    <ModifierPage name="Parabola" color="#EC368D" :template="template" icon="parabola">
+        <Slider track-color="#EC368D" :min="0.01" :max="1" :step="0.01" v-model="xParabola"
+            icon-left="parabola-flat"
+            icon-right="parabola-tall" 
+            @input="onSliderInput" 
+        />
+    </ModifierPage>
+</template>
+
---
+++ src/pages/Wave.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { ModifierPage, Slider } from '@/components';
+
+const constant = ref(1);
+const template = ref(`sin(${constant.value} * ({expr}))`);
+// "sin(5x)*cos(5y)/5"
+
+function onSliderInput() {
+    template.value = `sin(${constant.value} * ({expr}))`;
+}
+</script>
+
+<!---->
+
+<template>
+    <ModifierPage name="Wave" color="#4A5899" :template="template" icon="wave">
+        <Slider track-color="#4A5899" :min="-3.14" :max="3.14" :step="0.01" v-model="constant" @input="onSliderInput" />
+    </ModifierPage>
+</template>
