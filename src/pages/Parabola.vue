ecd8a0073454030aa2e5177e33cd39e6d47fc4cb
Mike K <30582227+zlrc@users.noreply.github.com>
2022-08-02 21:00:19 -0700

    refactor: updated Parabola.vue to omit '1' as a coefficient

--- src/pages/Parabola.vue
+++ src/pages/Parabola.vue
@@ -5,11 +5,12 @@ import { ModifierPage, Slider } from '@/components';
 const color = "#EC368D";
 
 const settings = ref<ModSettings>({
-    xParabola: 0.5
+    coef: 0.5
 });
 
 function process(expr : string, settings : ModSettings) {
-    return `${expr} + ${settings.xParabola}*x^2`.replace("0 + ", "");
+    const text = settings.coef === 1 ? "x^2" : `${settings.coef}x^2`;
+    return `${expr} + ${text}`.replace("0 + ", "");
 }
 </script>
 
@@ -17,7 +18,7 @@ function process(expr : string, settings : ModSettings) {
 
 <template>
     <ModifierPage name="Parabola" :color="color" :process-fn="process" icon="parabola" v-model="settings">
-        <Slider :track-color="color" :min="0.01" :max="1" :step="0.01" v-model="(settings.xParabola as number)"
+        <Slider :track-color="color" :min="0.01" :max="1" :step="0.01" v-model="(settings.coef as number)"
             icon-left="parabola-flat"
             icon-right="parabola-tall" 
         />
