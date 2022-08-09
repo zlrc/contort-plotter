d166477220802c9b7d2cf04850748f9a39ca99a1
Mike K <30582227+zlrc@users.noreply.github.com>
2022-08-08 22:10:39 -0700

    feat: added axis toggle to the Parabola modifier

--- src/pages/Parabola.vue
+++ src/pages/Parabola.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import { ModifierPage, Slider } from '@/components';
+import { AxisToggleButton, ModifierPage, Slider } from '@/components';
 
 const color = "#EC368D";
 
 const settings = ref<ModSettings>({
-    coef: 0.5
+    yAxis: false,
+    coef: 0.5,
 });
 
 function process(expr : string, settings : ModSettings) {
-    const text = settings.coef === 1 ? "x^2" : `${settings.coef}x^2`;
+    let v = settings.yAxis ? 'y' : 'x'; // which variable to use
+    const text = settings.coef === 1 ? `${v}^2` : `${settings.coef}${v}^2`;
     return `${expr} + ${text}`.replace("0 + ", "");
 }
 </script>
@@ -18,6 +20,9 @@ function process(expr : string, settings : ModSettings) {
 
 <template>
     <ModifierPage name="Parabola" :color="color" :process-fn="process" icon="parabola" v-model="settings">
+        <template v-slot:header>
+            <AxisToggleButton v-model="(settings.yAxis as boolean)" :color="color" />
+        </template>
         <Slider :track-color="color" :min="0.01" :max="1" :step="0.01" v-model="(settings.coef as number)"
             icon-left="parabola-flat"
             icon-right="parabola-tall" 
