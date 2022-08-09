c9a8c2ff5bc84a5945bc295220c17304fdae6c06
Mike K <30582227+zlrc@users.noreply.github.com>
2022-08-08 22:12:19 -0700

    feat: added a shift slider to Parabola

--- src/pages/Parabola.vue
+++ src/pages/Parabola.vue
@@ -7,10 +7,16 @@ const color = "#EC368D";
 const settings = ref<ModSettings>({
     yAxis: false,
     coef: 0.5,
+    shift: 0
 });
 
 function process(expr : string, settings : ModSettings) {
     let v = settings.yAxis ? 'y' : 'x'; // which variable to use
+    // Determine the operation to use (+/-) for shifting
+    if (settings.shift != 0) { // determine the sign to use for shifting
+        const shift = Math.abs(settings.shift as number);
+        v = settings.shift < 0 ? `(${v}-${shift})` : `(${v}+${shift})`;
+    } 
     const text = settings.coef === 1 ? `${v}^2` : `${settings.coef}${v}^2`;
     return `${expr} + ${text}`.replace("0 + ", "");
 }
@@ -24,9 +30,10 @@ function process(expr : string, settings : ModSettings) {
             <AxisToggleButton v-model="(settings.yAxis as boolean)" :color="color" />
         </template>
         <Slider :track-color="color" :min="0.01" :max="1" :step="0.01" v-model="(settings.coef as number)"
-            icon-left="parabola-flat"
-            icon-right="parabola-tall" 
-        />
+            icon-left="parabola-flat" icon-right="parabola-tall" />
+        &nbsp; &nbsp;
+        <Slider :track-color="color" :min="-1" :max="1" :step="0.01" v-model="(settings.shift as number)" 
+            icon-left="parabola-left" icon-right="parabola-right" />
     </ModifierPage>
 </template>
 
