6f557477fa6c3309c46811a76321715299ba9972
Mike K <30582227+zlrc@users.noreply.github.com>
2022-09-29 03:41:10 -0700

    feat: added coffee button

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ButtonGrid, ToolboxNavButton, ToolboxSection } from '@/components';
+import { ButtonGrid, RoundButton, ToolboxNavButton, ToolboxSection } from '@/components';
 import { menu } from '@/utils/constants';
 
 const emit = defineEmits<{
@@ -7,12 +7,19 @@ const emit = defineEmits<{
     (e: 'redirect', pageName: string): void
 }>();
 
+const coffeeURL = "https://ko-fi.com/A0A75O6SL";
+
 /**
  * Called when a button is clicked.
  * @param dest the destination page that the button leads to
  */
 function onClick(dest : string) {
     emit('redirect', dest);
+}
+
+/** Redirects the user to a page where they can leave a tip. */
+function onCoffeeButtonClick() {
+    window.open(coffeeURL, "_blank", "noopener");
 }
 </script>
 
@@ -31,6 +38,10 @@ function onClick(dest : string) {
             />
         </ButtonGrid>
     </ToolboxSection>
+    <RoundButton class="coffee-button" size="fit-content" fill-color="none" icon="heart" :title="coffeeURL" 
+        @click="onCoffeeButtonClick">
+        <span>Buy me a coffee!</span>
+    </RoundButton>
 </template>
 
 <!---->
@@ -38,5 +49,28 @@ function onClick(dest : string) {
 <style lang="scss">
 .button-grid__button {
     border-radius: $border-radius-global;
+}
+
+.coffee-button {
+    cursor: pointer;
+    display: block;
+
+    margin-left: auto;
+    margin-top: -5rem; // height + padding*2 + nav-button margin + body__content margin
+    padding: 0.5rem;
+
+    span {
+        margin: 0 0.5rem;
+    }
+
+    // Temporary hack to target the SVGIcon component
+    div { // TODO: provide a more specific selector
+        float: left;
+        height: 2rem;
+    }
+
+    @media (max-width: 480px) {
+        margin: 1rem 0;
+    }
 }
 </style>
