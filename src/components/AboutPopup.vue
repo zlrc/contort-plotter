255b7e06745f448d795028d2be80c0bebdf65032
Mike K <30582227+zlrc@users.noreply.github.com>
2022-09-29 03:42:28 -0700

    refactor: updated and added links to 'About' popup

--- src/components/AboutPopup.vue
+++ src/components/AboutPopup.vue
@@ -47,14 +47,19 @@ import { PopupOverlay } from '@/components';
             This project was created by Mike K. You are free to use any 3D visuals that
             you create with it in your own works or lessons, commercial or otherwise, as
             long as you link back to either this website or the source code repository.
-            Please refer to the repository's README file for exact information on licensing and
-            how this project can be used. Thank you.
+            Please refer to <a href="https://github.com/zlrc/contort-plotter#credits--licensing"
+            target="_blank" rel="noopener">the repository's README file</a> for exact information 
+            on licensing and how this project can be used. Thank you.
         </p>
         &nbsp;
         <template #footer>
-            [<a href="https://github.com/zlrc/contort-plotter" target="_blank" rel="noopener noreferrer">Source Code</a>] |
-            [<a href="https://fonts.google.com/share?selection.family=Gugi%7CNunito:ital,wght@0,400;0,700;1,400;1,700"
-                target="_blank" rel="noopener noreferrer">Fonts</a>]
+            <center>
+                © 2022 <a href="https://github.com/zlrc" target="_blank" rel="noopener noreferrer">Mike K.</a><br/>
+                [<a href="https://github.com/zlrc/contort-plotter" target="_blank" rel="noopener">Source Code</a>] 
+                [<a href="https://fonts.google.com/share?selection.family=Gugi%7CNunito:ital,wght@0,400;0,700;1,400;1,700"
+                    target="_blank" rel="noopener noreferrer">Fonts</a>] 
+                [<a href="https://github.com/zlrc/contort-plotter/issues" target="_blank" rel="noopener" style="white-space: nowrap">Issue Tracker</a>]
+            </center>
         </template>
     </PopupOverlay>
 </template>
